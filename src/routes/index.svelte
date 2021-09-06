06fb23cd439262916811522ffaf008dc880a35f9
erbridge <f@erbridge.co.uk>
2021-09-06 22:35:37 +0200

    Update changes to combatants

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,24 @@
 
 		combatants = [...combatants, blankCombatant];
 	}
+
+	function updateCombatant(index: number) {
+		return (event: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
+			combatants = [
+				...combatants.slice(0, index),
+				{
+					...combatants[index],
+					[event.currentTarget.name]:
+						event.currentTarget.type === 'number'
+							? event.currentTarget.value
+								? parseInt(event.currentTarget.value)
+								: null
+							: event.currentTarget.value
+				},
+				...combatants.slice(index + 1)
+			];
+		};
+	}
 </script>
 
 <h1>Dungeon Combat</h1>
@@ -23,20 +41,38 @@
 		<th>Hit points</th>
 	</tr>
 
-	{#each combatants as combatant}
+	{#each combatants as combatant, i}
 		<tr>
-			<td><input type="text" name="name" value={combatant.name} /></td>
-			<td><input type="number" name="initiative" value={combatant.initiative} /></td>
-			<td
-				><input
+			<td>
+				<input type="text" name="name" value={combatant.name} on:change={updateCombatant(i)} />
+			</td>
+			<td>
+				<input
+					type="number"
+					name="initiative"
+					value={combatant.initiative}
+					on:change={updateCombatant(i)}
+				/>
+			</td>
+			<td>
+				<input
 					type="number"
 					name="damage"
 					value={combatant.damage}
 					min="0"
 					max={combatant.hp}
-				/></td
-			>
-			<td><input type="number" name="hp" value={combatant.hp} min="1" /></td>
+					on:change={updateCombatant(i)}
+				/>
+			</td>
+			<td>
+				<input
+					type="number"
+					name="hp"
+					value={combatant.hp}
+					min="1"
+					on:change={updateCombatant(i)}
+				/>
+			</td>
 		</tr>
 	{/each}
 </table>
